9cc2735ff71789e29448e6b3695c57cbe56b280d
MuditMudgal <146031145+MuditMudgal@users.noreply.github.com>
2023-09-26 01:55:55 +0530

    Add files via upload

---
+++ assignment1.cpp
@@ -0,0 +1,66 @@
+//Ques1: How can you output “Physics”and  “Wallah” in two different lines in C++?
+
+#include<iostream>
+using namespace std;
+int main() {
+    cout<<"Physics"<<endl;
+    cout<<"Wallah";
+}
+
+
+
+//Ques2: Print 10 using 2 positive numbers less than 6  in C++ as output.
+
+#include<iostream>
+using namespace std;
+int main() {
+    cout<<5+5;
+}
+
+
+//Ques3: How much space does following data types take ?
+
+int     = 2byte
+bool    = 1bit
+float   = 4byte
+
+
+//Ques4: What is the output of this program?
+
+void main(){
+	int a=4;
+int b=5;
+a++;b--;
+cout<<++a<<” “<<b–-;
+}
+
+output of this program : 64
+
+
+
+//Ques5: WAP to find the circumference of a circle with radius 10 in C++.
+
+#include<iostream>
+using namespace std;
+int main() {
+	int radius = 10;
+	float pi = 3.1415;
+	float circumference = 2*pi*radius;
+	
+	cout<<" circumference is :"<<circumference;
+}
+
+
+
+
+//Ques6: How many of these can be a variable name?
+
+01PWskills         = NO
+ 
+_FLOAT             = YES
+
+int                = NO
+
+FLOAT              = YES
+
+You will succeed   = NO
